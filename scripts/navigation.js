1550f57d446122286628ae788748b95eb1dae73a
himanshumps <himanshu.mps@gmail.com>
2019-02-19 13:45:51 +0530

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,22 +1,34 @@
-window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.splice(
-  OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.length,
-  0,
-  {
-    id: 'openshiftplusplus',
-    label: 'Openshift++',
-    subCategories: [
-      { 
-         id: 'platformadmin',
-         label: 'Platform Admin',
-         tags: ['go'],
-         icon: 'fa fa-mobile'
-      },
-      {
-        id: 'platformdeveloper',
-        label: 'Platform Developer',
-        tags: ['go'],
-        icon: 'fa fa-database'
-      }
-    ]
-  }
-);
+// Add a Featured category as the first category tab.
+window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.unshift({
+  // Required. Must be unique.
+  id: "featured",
+  // Required
+  label: "Featured",
+  subCategories: [
+    {
+      // Required. Must be unique.
+      id: "go",
+      // Required.
+      label: "Go",
+      // Optional. If specified, defines a unique icon for this item.
+      icon: "icon-go-gopher",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
+        "go",
+        "golang"
+      ]
+    },
+    {
+      // Required. Must be unique.
+      id: "jenkins",
+      // Required.
+      label: "Jenkins",
+      // Optional. If specified, defines a unique icon for this item.
+      icon: "icon-jenkins",
+      // Required. Items matching any tag will appear in this subcategory.
+      tags: [
+        "jenkins"
+      ]
+    }
+  ]
+});
