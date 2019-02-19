8fef2a43402d35465414f8d27689c8d3652fb4dd
himanshumps <himanshu.mps@gmail.com>
2019-02-19 13:28:05 +0530

    Create navigation.js

---
+++ scripts/navigation.js
@@ -0,0 +1,22 @@
+window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.splice(
+  OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.length,
+  0,
+  {
+    id: 'openshiftplusplus',
+    label: 'Openshift++',
+    subCategories: [
+      { 
+         id: 'platformadmin',
+         label: 'Platform Admin',
+         tags: ['go'],
+         icon: 'fa fa-mobile'
+      },
+      {
+        id: 'platformdeveloper',
+        label: 'Platform Developer',
+        tags: ['go'],
+        icon: 'fa fa-database'
+      }
+    ]
+  }
+);
