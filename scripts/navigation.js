838992b865c72b59230d92089bf5db347fab0323
himanshumps <himanshu.mps@gmail.com>
2019-02-19 14:54:18 +0530

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,19 +1,19 @@
 window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.splice(
-	OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.length,
+	OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.length-1,
 	0, {
 		id: 'openshiftplusplus',
-		label: 'Openshift++',
+		label: 'Openshift Plus Plus',
 		subCategories: [{
 				id: 'platformadmin',
 				label: 'Platform Admin',
-				tags: ['go'],
-				icon: 'fa fa-mobile'
+				tags: ['go','golang'],
+				icon: 'icon-go-gopher'
 			},
 			{
 				id: 'platformdeveloper',
 				label: 'Platform Developer',
-				tags: ['go'],
-				icon: 'fa fa-database'
+				tags: ['go','golang'],
+				icon: 'icon-go-gopher'
 			}
 		]
 	}
