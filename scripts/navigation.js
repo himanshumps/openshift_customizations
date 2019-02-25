bcde1c627674ca06131b9fc176997895bb5f0430
himanshumps <himanshu.mps@gmail.com>
2019-02-25 15:52:05 +0530

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -19,15 +19,19 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.splice(
 	}
 );
 
-angular
-  .module('aboutPageExtension', ['openshiftConsole'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/test', {
-        templateUrl: 'https://himanshumps.github.io/openshift_customizations/scripts/index.html',
-        controller: 'AboutController'
-      });
-    }
-  );
-
-hawtioPluginLoader.addModule('aboutPageExtension');
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
+  title: "Dashboard",                         // The text label
+  icon: "fa fa-dashboard",                    // The icon you want to appear
+  url: "http://example.com/dashboard",        // Where to go when this item is clicked
+  description: "Open application dashboard."  // Short description
+}, {
+  title: "System Status",
+  icon: "fa fa-heartbeat",
+  url: "http://example.com/status",
+  description: "View system alerts and outages."
+}, {
+  title: "Manage Account",
+  icon: "pficon pficon-user",
+  url: "http://example.com/account",
+  description: "Update email address or password."
+}];
