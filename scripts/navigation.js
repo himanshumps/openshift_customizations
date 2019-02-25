e0cf8efc5fcc1be0e7c3c634ed1298557c0043fd
himanshumps <himanshu.mps@gmail.com>
2019-02-25 16:32:56 +0530

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -20,18 +20,8 @@ window.OPENSHIFT_CONSTANTS.SERVICE_CATALOG_CATEGORIES.splice(
 );
 
 window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
-  title: "Dashboard",                         // The text label
-  icon: "fa fa-dashboard",                    // The icon you want to appear
+  title: "Openshift ++",                         // The text label
+  icon: "fa fa-snowflake-o",                    // The icon you want to appear
   url: "http://example.com/dashboard",        // Where to go when this item is clicked
-  description: "Open application dashboard."  // Short description
-}, {
-  title: "System Status",
-  icon: "fa fa-heartbeat",
-  url: "http://example.com/status",
-  description: "View system alerts and outages."
-}, {
-  title: "Manage Account",
-  icon: "pficon pficon-user",
-  url: "http://example.com/account",
-  description: "Update email address or password."
+  description: "The one shop for creating the E2E microservices application"  // Short description
 }];
